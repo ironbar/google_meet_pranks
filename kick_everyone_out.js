e41eb18cd1fc42a89181f446c1bae8236942ef56
gbarbadillo <gbarbadillo@das-nano.com>
2023-07-21 17:55:51 +0200

    updated kick everyone out

--- kick_everyone_out.js
+++ kick_everyone_out.js
@@ -1,7 +1,15 @@
-document.querySelectorAll('[aria-label="More actions"]')[1].click();
-setTimeout(() => document.querySelector('[aria-label="Remove from the call"]').click(), 200);
-setTimeout(
-  () =>
-    document.querySelector('[aria-label="Remove"]').click(),
-  600
-);
+function kick_everyone_out(){
+  let timeout = 0;
+  people = document.querySelectorAll('[aria-label="More actions"]');
+  // loop over all the people excluding the first one
+  for (let i = 1; i < people.length; i++) {
+    setTimeout(() => people[i].click(), timeout);
+    timeout += 200;
+    setTimeout(() => document.querySelector('[aria-label="Remove from the call"]').click(), timeout);
+    timeout += 400;
+    setTimeout(() => document.querySelector('[aria-label="Remove"]').click(), timeout);
+    timeout += 400;
+  }
+}
+kick_everyone_out();
+
