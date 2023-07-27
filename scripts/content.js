78eb73e482c2feb68e58988b7c4bd1e655aa7b8f
gbarbadillo <gbarbadillo@das-nano.com>
2023-07-27 18:07:13 +0200

    added thumbs down function and random execution function

--- scripts/content.js
+++ scripts/content.js
@@ -20,16 +20,37 @@ function raiseHand() {
   }
 }
 
+function thumbsDown() {
+    console.log("Thumbs down");
+    reactions_menu = document.querySelector('[aria-label="Reactions"]');
+    if (reactions_menu) {
+        getDeepFirstChild(reactions_menu.childNodes[8]).click();
+        return;
+    }
+}
+
 function randomIntFromInterval(min, max) {
     return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
-let initial_delay = randomIntFromInterval(5, 10);
+
+function executeRandomFunction(functions) {
+  var functions = [raiseHand, thumbsDown];
+  var index = randomIntFromInterval(0, functions.length - 1);
+  functions[index]();
+}
+
+
+
+let initial_delay = randomIntFromInterval(1, 3);
 let delay = initial_delay;
 let n_actions = 10;
 
 for (let i = 0; i < n_actions; i++) {
-  delay += randomIntFromInterval(2, 5);
-  setTimeout(raiseHand, delay * 1000);
+  delay += randomIntFromInterval(1, 3);
+  setTimeout(
+    executeRandomFunction,
+    delay * 1000
+  );
   console.log("I will raise the hand in " + delay + " seconds.");
 }
