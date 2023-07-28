a4392f93446be781d605905f4c87fb5ad12ab1d7
gbarbadillo <gbarbadillo@das-nano.com>
2023-07-28 14:51:36 +0200

    added toggle camera and microphone

--- scripts/content.js
+++ scripts/content.js
@@ -18,10 +18,21 @@ function clickOnFirstAvailableButton(names) {
 }
 
 
-function raiseHand() {
-  console.log("Raise hand");
+function toggleHand() {
+  console.log("Toggling hand");
   clickOnFirstAvailableButton(["Raise hand (Ctrl + alt + h)", "Lower hand (Ctrl + alt + h)"]);
 }
+
+function toggleMicrophone() {
+  console.log("Toggling microphone");
+  clickOnFirstAvailableButton(["Turn off microphone (CTRL + D)", "Turn on microphone (CTRL + D)"]);
+}
+
+function toggleCamera() {
+  console.log("Toggling camera");
+  clickOnFirstAvailableButton(["Turn on camera (CTRL + E)", "Turn off camera (CTRL + E)"]);
+}
+
 
 function thumbsDown() {
     console.log("Thumbs down");
@@ -38,7 +49,7 @@ function randomIntFromInterval(min, max) {
 
 
 function executeRandomFunction(functions) {
-  var functions = [raiseHand, thumbsDown];
+  var functions = [toggleHand, thumbsDown, toggleMicrophone, toggleCamera];
   var index = randomIntFromInterval(0, functions.length - 1);
   functions[index]();
 }
@@ -47,9 +58,9 @@ function executeRandomFunction(functions) {
 function doRandomActionsOnGoogleMeet() {
   let initial_delay_m = randomIntFromInterval(5, 10);
   let delay_m = initial_delay_m;
-  let n_actions = 10;
+  let n_actions = 100;
   for (let i = 0; i < n_actions; i++) {
-    setTimeout(executeRandomFunction, delay_m * 1000);
+    setTimeout(executeRandomFunction, delay_m * 60000);
     console.log("Next action in " + delay_m + " minutes");
     delay_m += randomIntFromInterval(1, 10);
   }
